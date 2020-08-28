25c3dddce2b377937bacffa51de256408cfb1246
Adrian Ludlow <aludlow@powercor.com.au>
2020-08-28 17:05:45 +1000

    Added ComponentManager class.

--- src/engine/core/ComponentManager.h
+++ src/engine/core/ComponentManager.h
@@ -2,28 +2,69 @@
 
 #include <unordered_map>
 #include <memory>
+#include <typeinfo>
+#include <assert.h>
 
 #include "types.h"
+#include "ComponentArray.h"
 
 class ComponentManager {
   public:
     template<typename T>
-    void registerComponent();
+    void registerComponent() {
+      const char* typeName = typeid(T).name();
+
+      assert(componentTypes_.find(typeName) == componentTypes_.end() && "Attempted to register component more than once.");
+
+      // Add new component to maps
+      componentTypes_.insert({ typeName, nextComponentType_ });
+      componentArrays_.insert({ typeName, std::make_unique<ComponentArray<T>>() });
+
+      ++nextComponentType_;
+    }
 
     template<typename T>
-    void getComponentType();
+    ComponentType getComponentType() {
+      const char* typeName = typeid(T).name();
+
+      assert(componentTypes_.find(typeName) != componentTypes_.end() && "Requested unregistered component type.");
+
+      return componentTypes_[typeName];
+    }
 
     template<typename T>
-    void addComponent(Entity entity, T component);
+    void addComponent(Entity entity, T component) {
+      GetComponentArray<T>()->addComponent(entity, component);
+    }
 
     template<typename T>
-    void removeComponent(Entity entity);
+    void removeComponent(Entity entity) {
+      GetComponentArray<T>()->removeComponent(entity);
+    }
 
     template<typename T>
-    T& getComponent(Entity entity);
+    T& getComponent(Entity entity) {
+      return GetComponentArray<T>()->getComponent(entity);
+    }
 
-    void entityDeleted(Entity entity);
+    void entityDeleted(Entity entity) {
+      for (auto const& pair : componentArrays_) {
+        auto const& component = pair.second;
+        component->entityDeleted(entity);
+      }
+    }
 
   private:
-    std::unordered_map<const char*, std::unique_ptr<IComponentArray>> componentArrays {};
+    std::unordered_map<const char*, ComponentType> componentTypes_ {};
+    std::unordered_map<const char*, std::unique_ptr<IComponentArray>> componentArrays_ {};
+    ComponentType nextComponentType_ {};
+
+    template<typename T>
+    std::unique_ptr<ComponentArray<T>> GetComponentArray() {
+      const char* typeName = typeid(T).name();
+
+      assert(componentTypes_.find(typeName) != componentTypes_.end() && "Requested unregistered component type.");
+
+      return std::static_pointer_cast<ComponentArray<T>>(componentArrays_[typeName]);
+    }
 };
--- src/engine/core/types.h
+++ src/engine/core/types.h
@@ -5,3 +5,7 @@
 // Entity
 using Entity = std::uint32_t;
 const Entity MAX_ENTITIES = 10000;
+
+//ComponentType
+using ComponentType = std::uint16_t;
+const ComponentType MAX_COMPONENTS = 64;

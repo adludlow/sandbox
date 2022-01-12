7c3e471797c0fd71b1e9d0a438abbf130185ba9c
aludlow <aludlow@gmail.com>
2022-01-12 21:02:01 +1100

    WIP: Text rendering

--- src/engine/systems/TextRenderSystem.h
+++ src/engine/systems/TextRenderSystem.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <string>
 #include <map>
+#include <iostream>
 
 #include <glm/gtc/matrix_transform.hpp>
 #include <ft2build.h>
@@ -37,7 +38,59 @@ class TestRenderSystem : public System {
     void loadFont(const std::string& font, const unsigned int fontSize) {
       std::map<char, Character> characters {};
       FT_Library ft;
+      if (FT_Init_FreeType(&ft)) {
+        std::cerr << "ERROR::FREETYPE: Failed to initialise Freetype Library" << std::endl;
+      }
 
+      FT_Face face;
+      if (FT_New_Face(ft, font.c_str(), 0, &face)) {
+        std::cerr << "ERROR::FREETYPE: Failed to load font: " << font << std::endl;
+      }
+
+      // Set font size
+      FT_Set_Pixel_Sizes(face, 0, fontSize);
+      // Disable byte-alignment restriction
+      glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+      for (GLubyte c = 0; c < 128; c++) {
+        // Load character glyph
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+          std::cerr << "ERROR::FREETYPE: Failed to load glyph" << std::endl;
+          continue;
+        }
+        // Generate texture
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+          GL_TEXTURE_2D,
+          0,
+          GL_RED,
+          face->glyph->bitmap.width,
+          face->glyph->bitmap.rows,
+          0,
+          GL_RED,
+          GL_UNSIGNED_BYTE,
+          face->glyph->bitmap.buffer
+        );
+        // Set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // Store character for later use
+        Character character = {
+          texture,
+          glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+          glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+          face->glyph->advance.x
+        };
+        characters.insert(std::pair<char, Character>(c, character));
+      }
+      glBindTexture(GL_TEXTURE_2D, 0);
+      // Cleanup
+      FT_Done_Face(face);
+      FT_Done_FreeType(ft);
     }
 
     void init() {
